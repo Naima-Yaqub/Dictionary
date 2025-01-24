2fbed7adaea947368a356aa6f12a446e8cc81ed0
Naima-Yaqub <130101867+Naima-Yaqub@users.noreply.github.com>
2025-01-24 19:25:42 +0500

    Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,5 +1,10 @@
 import json 
 import streamlit as st 
+from difflib import SequenceMatcher
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
 try:
  data= json.load(open("data.json",'r'))
 except:
@@ -15,14 +20,17 @@ values= list(data.values())
 value= []
 if entry and data:
  for key in keys:
-     if entry[0]==key[0]:
-         if entry==key:
-             value= values[keys.index(key)]
+     
+     if entry==key:
+        value= values[keys.index(key)]
+     elif similar(key,entry)>=0.85:
+        st.write(f"Similar key found: '{key}' with similarity {similar(key,entry):.2f}")
+        value= values[keys.index(key)]
  if isinstance(value,list):
     formatted_definitions= "\n".join([f"{val}" for val in value])
     st.text_area(label='Output Data', value=f'The meanings of {entry} are:\n {formatted_definitions }', height= 200)            
  elif value:
-  st.text_area(label='Output Data', value=f'The meaning of {entry} is:\n {value }', height= 200)
+  st.text_area(label=f'Output Data the meaning of {entry} is:', value=f'{value }', height= 200)
  else:
   st.text_area(label='Output Data', value='No match found!')
 else:
